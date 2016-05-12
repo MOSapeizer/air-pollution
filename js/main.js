c78198ad92c7c1b5e225108c9db713641caa64d2
MOSapeizer <k471352@gmail.com>
2016-05-12 10:11:56 +0800

    New sources of images and data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 d3.json("data/county.json", function(topodata) {
   var features = topojson.feature(topodata, topodata.objects["County_MOI_1041215"]).features;
+  var path = d3.geo.path().projection(
+    d3.geo.mercator().center([121,24]).scale(8000)
+  );
+  	d3.select("svg").selectAll("path").data( features )
+  		.enter().append("path").attr("d", path);
 });
